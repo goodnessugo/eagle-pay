30b54cb8354386934bef00cb3b796573246aa84b
goodnessugo <emusical123@gmail.com>
2025-01-22 01:01:55 +0100

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import Navigation from './assets/components/Navigation'
+import Overview from './assets/components/Overview'
+import Heading from './assets/components/Heading'
+import SideNavigation from './assets/components/SideNavigation'
+import Transaction from './assets/components/Transaction'
+import Bills from './assets/components/Bills'
+import Transfer from './assets/components/Transfer'
+import CustomerTransaction from './assets/components/CustomerTransaction'
+import BillsCollection from './assets/components/BillsCollection'
+import Airtime from './assets/components/Airtime'
+import AddMoney from './assets/components/AddMoney'
+import Balance from './assets/components/Balance'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className=' '>
+
+      <BrowserRouter>
+        <Heading />
+
+        {/* testing linking */}
+        
+        {/* end of testing linking */}
+        <Routes>
+
+          <Route path="/" element={<Overview />} />
+          <Route path="/navigation" element={<Navigation />} />
+          <Route path="/heading" element={<Heading />} />
+          <Route path="/sidenavigation" element={<SideNavigation />} />
+          <Route path="/transaction" element={<Transaction />} />
+          <Route path="/bills" element={<Bills />} />
+          <Route path="/transfer" element={<Transfer />} />
+          <Route path="/customertransaction" element={<CustomerTransaction />} />
+          <Route path="/billscollection" element={<BillsCollection />} />
+          <Route path="/airtime" element={<Airtime />} />
+          <Route path="/addmoney" element={<AddMoney />} />
+          <Route path="/balance" element={<Balance />} />
+         
+        </Routes>
+        <Navigation />
+      </BrowserRouter>
+
+
+
+
+    </div>
   )
 }
 
